328d29d5ed54e5ff9afd02c9f9da827d43152ff5
t <142580138+tasfias@users.noreply.github.com>
2025-10-04 03:56:04 +0100

    Delete

--- session_2/arrays.js
+++
@@ -1,7 +0,0 @@
-const ingredients = ["chicken", "butter", "salt", "pepper"];
-const steps = [];
-ingredients.push("oregano");
-ingredients.push("mozerella");
-const lastIngredient = ingredients.pop();
-console.log(ingredients);
-console.log(lastIngredient);
